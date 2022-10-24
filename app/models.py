79206af69c9cb634a3413160b5036bf271569a07
ndestrieux <n.destrieux@gmail.com>
2022-10-24 21:30:25 +0200

    User model overwritten so email is set as username automatically

--- app/models.py
+++ app/models.py
@@ -1,9 +1,33 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from app.enums import LanguageChoice, VoteChoice
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password),
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email_address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
 
 
 class Recipe(models.Model):
