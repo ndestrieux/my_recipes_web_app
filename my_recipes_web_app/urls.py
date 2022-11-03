31deb3846d39bd17e61cf63ca729edb10bd529f3
ndestrieux <n.destrieux@gmail.com>
2022-11-03 16:15:27 +0100

    UserRegistrationView added to URLs

--- my_recipes_web_app/urls.py
+++ my_recipes_web_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import UserRegistrationView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/", UserRegistrationView.as_view(), name="register"),
 ]
