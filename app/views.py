563237d68c1e6575140f677f704bbd3e3ed98cc7
ndestrieux <n.destrieux@gmail.com>
2022-11-03 16:15:02 +0100

    UserRegistrationView created

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from app.forms import UserRegistrationForm
+
+
+class UserRegistrationView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = "users/registration.html"
+    success_url = reverse_lazy("register")
