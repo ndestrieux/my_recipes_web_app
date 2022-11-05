371fd401227d81c910f89a041c6ea7941d6469f2
ndestrieux <n.destrieux@gmail.com>
2022-11-05 16:55:24 +0100

    User registration form and view customed

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, ButtonHolder, HTML
+
 
 class UserRegistrationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
@@ -8,6 +11,26 @@ class UserRegistrationForm(UserCreationForm):
         self.fields["first_name"].required = True
         self.fields["last_name"].required = True
         self.fields["email"].required = True
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.layout = Layout(
+            Row(
+                Column("first_name", css_class="form-group col-md-6"),
+                Column("last_name", css_class="form-group col-md-6"),
+                css_class="row"
+            ),
+            "username",
+            "email",
+            Row(
+                Column("password1", css_class="form-group col-md-6"),
+                Column("password2", css_class="form-group col-md-6"),
+                css_class="row"
+            ),
+            ButtonHolder(
+                Submit('submit', 'register'),
+                HTML("<a href='#' class='btn btn-primary'>Cancel</a>")
+            )
+        )
 
     class Meta:
         model = User
