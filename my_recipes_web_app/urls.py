f0b1d6eb7159c7f564d3487c28b6a2c140e3cb0b
ndestrieux <n.destrieux@gmail.com>
2022-11-06 16:37:40 +0100

    Logout view added

--- my_recipes_web_app/urls.py
+++ my_recipes_web_app/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from app.views import UserRegistrationView, UserLoginView
 
@@ -21,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
