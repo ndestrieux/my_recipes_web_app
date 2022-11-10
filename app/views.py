d751b118ba593f0af9cfb21d409ab5ed2fdff6f7
ndestrieux <n.destrieux@gmail.com>
2022-11-10 22:44:17 +0100

    Recipe creation view created

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
-from django.contrib.auth.views import LoginView
+from extra_views import CreateWithInlinesView, NamedFormsetsMixin
 
-from app.forms import UserRegistrationForm, UserLoginForm
-from app.models import Recipe
+from app.forms import (IngredientQuantityFormSet, RecipeForm, UserLoginForm,
+                       UserRegistrationForm)
+from app.models import Ingredient, Recipe
 
 
 class UserRegistrationView(CreateView):
@@ -21,3 +24,25 @@ class UserLoginView(LoginView):
 class HomePageView(ListView):
     model = Recipe
     template_name = "app/home_page.html"
+
+
+class RecipeCreationView(LoginRequiredMixin, NamedFormsetsMixin, CreateWithInlinesView):
+    model = Recipe
+    form_class = RecipeForm
+    inlines = [
+        IngredientQuantityFormSet,
+    ]
+    inlines_names = [
+        "ingredient_forms",
+    ]
+    template_name = "app/create_recipe.html"
+    success_url = reverse_lazy("home")
+
+    def get_context_data(self, **kwargs):
+        kwargs["ingredients"] = Ingredient.objects.all()
+        return super().get_context_data(**kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["current_user_id"] = self.request.user.id
+        return kwargs
