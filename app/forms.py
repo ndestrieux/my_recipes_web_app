80894edbb149851d43951a73a06340a2f710a212
ndestrieux <n.destrieux@gmail.com>
2022-11-10 22:42:54 +0100

    Recipe form along with IngredientQuantity inline formset created for recipe creation view

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Column, ButtonHolder, HTML
+from crispy_forms.layout import (HTML, ButtonHolder, Column, Layout,
+                                 Row, Submit)
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from extra_views import InlineFormSetFactory
+
+from app.models import Ingredient, IngredientQuantity, Recipe
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -61,3 +65,85 @@ class UserLoginForm(AuthenticationForm):
         class Meta:
             model = User
             fields = ["username", "password"]
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = [
+            "name",
+            "content",
+            "nb_of_people",
+            "image",
+            "language",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        self.current_user_id = kwargs.pop("current_user_id")
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.posted_by = User.objects.get(pk=self.current_user_id)
+        if commit:
+            instance.save()
+        return instance
+
+
+class IngredientQuantityForm(forms.ModelForm):
+    ingredient = forms.CharField(
+        widget=forms.TextInput(attrs={"type": "hidden"}), required=False
+    )
+    ingredient_value = forms.CharField(
+        label="Ingredient", widget=forms.TextInput(attrs={"list": "ingredients"})
+    )
+
+    class Meta:
+        model = IngredientQuantity
+        fields = [
+            "ingredient",
+            "ingredient_value",
+            "quantity",
+            "unit",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                Column(
+                    "ingredient",
+                    "ingredient_value",
+                    HTML(
+                        """<datalist id="ingredients">
+                            {% for ingredient in ingredients %}
+                                <option id="{{ ingredient.id }}" value="{{ ingredient.name }}">
+                            {% endfor %}
+                        </datalist>"""
+                    ),
+                    css_class="form-group col-md-4",
+                ),
+                Column("quantity", css_class="form-group col-md-4"),
+                Column("unit", css_class="form-group col-md-4"),
+                css_class="row",
+            ),
+        )
+
+    def clean(self):
+        ingredient = self.cleaned_data["ingredient"]
+        if ingredient:
+            ingredient = Ingredient.objects.get(id=ingredient)
+        else:
+            ingredient = Ingredient.objects.create(
+                name=self.cleaned_data["ingredient_value"]
+            )
+        self.cleaned_data["ingredient"] = ingredient
+        return self.cleaned_data
+
+
+class IngredientQuantityFormSet(InlineFormSetFactory):
+    model = IngredientQuantity
+    form_class = IngredientQuantityForm
+    factory_kwargs = {"extra": 1, "can_delete": False}
