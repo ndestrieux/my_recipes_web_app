42481122c50dc020dfb2c4fedf1a9df35f9c4393
ndestrieux <n.destrieux@gmail.com>
2022-11-11 23:13:30 +0100

    js script for datalist fixed so it responds better to inline forms

--- app/static/js/datalist-custom.js
+++ app/static/js/datalist-custom.js
@@ -1,6 +1,8 @@
 $(function() {
-    $("input[list=ingredients]").on("input",function() {
+    $("form").on("input", "input[list=ingredients]",function()   {
+        console.log($(this));
         var selectedOption = $('option[value="'+$(this).val()+'"]');
         var ingredientFormField = $(this).parents().eq(1).prev("input");
+        ingredientFormField.val(selectedOption.length ? selectedOption.attr('id') : "");
     });
 });
