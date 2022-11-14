806e2d6eae327d1b05506499b982a6ac573a63a4
ndestrieux <n.destrieux@gmail.com>
2022-11-14 15:00:59 +0100

    Fix applied on form count number to avoid to have 2 forms with the same number

--- app/static/js/inline-form-management.js
+++ app/static/js/inline-form-management.js
@@ -5,11 +5,12 @@ $(function () {
         $("form").on("click", ".add_form", function (event) {
             const formSetDiv = $(this).parents(".form_set");
             const totalForms = formSetDiv.children("input:first");
+            const totalFormsCreated = parseInt(totalForms.val());
             const emptyForm = formSetDiv.children(".empty_form");
-            newForm = $.parseHTML(emptyForm.html().replace(/__prefix__/g, form_count))
+            newForm = $.parseHTML(emptyForm.html().replace(/__prefix__/g, totalFormsCreated))
             emptyForm.before(newForm)
             form_count++;
-            totalForms.val(form_count);
+            totalForms.val(totalFormsCreated + 1);
             if (form_count > 1) {
                 formSetDiv.children(".ingredient-container:first").children(".remove_form:first").show();
             }
