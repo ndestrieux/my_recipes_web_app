40df5341272c787ef6172381c3176e7e8ff5dafc
ndestrieux <n.destrieux@gmail.com>
2022-11-17 19:30:57 +0100

    If page gets reloaded and ingredient has been created, it will autofilled ID field with the ID newly created

--- app/static/js/datalist-custom.js
+++ app/static/js/datalist-custom.js
@@ -1,7 +1,19 @@
+$(window).on('load', function() {
+    var ingredientValueField = $("input[list=ingredients]");
+    ingredientValueField.each(function () {
+        var ingredientFormField = $(this).parents().eq(1).prev("input");
+        var selectedOption = $('option[value="'+$(this).val()+'"]');
+        if (ingredientFormField.val() === "")   {
+            ingredientFormField.val(selectedOption.attr("id"));
+        }
+    });
+});
+
+
 $(function() {
     $("form").on("input", "input[list=ingredients]",function()   {
         var selectedOption = $('option[value="'+$(this).val()+'"]');
         var ingredientFormField = $(this).parents().eq(1).prev("input");
-        ingredientFormField.val(selectedOption.length ? selectedOption.attr('id') : "");
+        ingredientFormField.val(selectedOption.length ? selectedOption.attr("id") : "");
     });
 });
