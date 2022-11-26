9e5945d3681f0049c4e13b774b518792eb998bf3
ndestrieux <n.destrieux@gmail.com>
2022-11-26 15:59:14 +0100

    signals fixed to take into account updated and deleted instance count

--- app/signals.py
+++ app/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from app.enums import VoteChoice
@@ -12,11 +12,24 @@ def create_ranking(sender, instance, created, **kwargs):
 
 
 @receiver(post_save, sender=VoteHistory)
-def update_ranking(sender, instance, created, **kwargs):
-    if created:
-        ranking = Ranking.objects.get(recipe=instance.recipe)
-        if instance.vote == VoteChoice.UP:
-            ranking.up += 1
-        elif instance.vote == VoteChoice.DOWN:
-            ranking.down += 1
-        ranking.save()
+def update_ranking_on_vote(sender, instance, update_fields, **kwargs):
+    ranking = Ranking.objects.get(recipe=instance.recipe)
+    if instance.vote == VoteChoice.UP.name:
+        ranking.up += 1
+        if update_fields:
+            ranking.down -= 1
+    elif instance.vote == VoteChoice.DOWN.name:
+        ranking.down += 1
+        if update_fields:
+            ranking.up -= 1
+    ranking.save()
+
+
+@receiver(post_delete, sender=VoteHistory)
+def update_ranking_on_vote_delete(sender, instance, **kwargs):
+    ranking = Ranking.objects.get(recipe=instance.recipe)
+    if instance.vote == VoteChoice.UP.name:
+        ranking.up -= 1
+    elif instance.vote == VoteChoice.DOWN.name:
+        ranking.down -= 1
+    ranking.save()
