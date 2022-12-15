b9939f3220707e6efe2394d91f109da4b87952d0
ndestrieux <n.destrieux@gmail.com>
2022-12-15 17:40:38 +0100

    send_mail functionality improved with a EmailTemplate class and default from_email registered in settings

--- app/tasks.py
+++ app/tasks.py
@@ -1,22 +1,22 @@
 from celery import shared_task
+from django.conf import settings
 from django.core.mail import EmailMessage
 
 from app.properties import PDF_TEMPLATE
-from app.utils import render_to_pdf
+from app.utils import render_to_pdf, EmailTemplate
 
 
 @shared_task(autoretry_for=(Exception,), default_retry_delay=30, max_retries=3)
 def send_email_task(username, recipient, content, context):
     pdf = render_to_pdf(PDF_TEMPLATE, context)
     pdf_name = f"{context['recipe']['name']}.pdf"
-    subject = f"{username} shared a recipe with you!"
-    separator = 40 * "-"
-    base_content = f"\n\n{separator}\nPlease find the recipe in attachment"
-    content = content + base_content
+    template = EmailTemplate()
+    subject = template.get_full_subject(username)
+    content = template.get_full_content(content)
     email = EmailMessage(
         subject=subject,
         body=content,
-        from_email="n.destrieux@gmail.com",
+        from_email=settings.FROM_EMAIL,
         to=[
             recipient,
         ],
