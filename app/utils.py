0d4b8b0c71cfa32a09b7888a7de23a3de4e4b489
ndestrieux <n.destrieux@gmail.com>
2022-12-15 16:59:13 +0100

    render_to_pdf function fixed

--- app/utils.py
+++ app/utils.py
@@ -5,12 +5,16 @@ from django.template.loader import get_template
 from xhtml2pdf import pisa
 
 
-def render_to_pdf(template_src, context_dict={}):
+
+def render_to_pdf(template_src, context_dict=None):
+    if context_dict is None:
+        context_dict = {}
     template = get_template(template_src)
-    html  = template.render(context_dict)
+    html = template.render(context_dict)
     buffer = BytesIO()
     status = pisa.pisaDocument(BytesIO(html.encode("utf-8")), buffer)
-    pdf_file = buffer.getvalue()
+    buffer.seek(0)
+    pdf_file = buffer.read()
     if not status.err:
         return pdf_file
     return None
