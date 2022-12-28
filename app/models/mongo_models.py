b87cd84b1b6c455210a35135c4cdcd367a97efea
ndestrieux <n.destrieux@gmail.com>
2022-12-28 12:17:01 +0100

    Modification done on MailLogs model

--- app/models/mongo_models.py
+++ app/models/mongo_models.py
@@ -3,11 +3,10 @@ from djongo import models as mongo_models
 
 class MailLogs(mongo_models.Model):
     from_user_id = mongo_models.IntegerField()
-    recipient = mongo_models.CharField(max_length=256)
+    recipient = mongo_models.EmailField()
     recipe_id = mongo_models.IntegerField()
-    status = mongo_models.CharField(max_length=256)
-    timestamp = mongo_models.DateTimeField(blank=True, null=True)
+    sent = mongo_models.BooleanField()
+    timestamp = mongo_models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        _use_db = "mongodb"
         ordering = ["-timestamp"]
