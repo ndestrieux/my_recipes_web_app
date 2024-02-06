d50dd027fafba34518bc2c07dda14d987d404d48
ndestrieux <n.destrieux@gmail.com>
2024-02-06 17:51:55 +0100

    Media storage configured on S3.

--- src/my_recipes_web_app/settings.py
+++ src/my_recipes_web_app/settings.py
@@ -143,24 +143,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images) / Media files
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_FILES = "media/"
-MEDIA_ROOT = BASE_DIR / "media/"
-
-USE_S3 = env.bool('USE_S3', False)
+USE_S3 = env.bool("USE_S3", False)
 
 if USE_S3:
     # aws settings
     AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read"
+    AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-central-1.amazonaws.com"
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     # s3 static settings
     AWS_LOCATION = "static"
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+    STATICFILES_STORAGE = "my_recipes_web_app.storage_backends.StaticStorage"
+    # s3 public media settings
+    MEDIA_LOCATION = "media"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/"
+    DEFAULT_FILE_STORAGE = "my_recipes_web_app.storage_backends.MediaStorage"
 else:
-    STATIC_ROOT = BASE_DIR / "static/"
     STATIC_URL = "static/"
+    STATIC_ROOT = BASE_DIR / "static/"
+    MEDIA_FILES = "media/"
+    MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
